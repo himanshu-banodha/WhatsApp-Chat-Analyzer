ce443a5c75c995891bdd7ce2ba5220e990480a1f
Himanshu Banodha <161852954+himanshu-banodha@users.noreply.github.com>
2024-06-19 23:46:00 +0530

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,51 @@
+import re
+import pandas as pd
+
+def preprocess(data):
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    # convert message_date type
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+
+    df.rename(columns={'message_date': 'datetime'}, inplace=True)
+
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:  # user name
+            users.append(entry[1])
+            messages.append(" ".join(entry[2:]))
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['messages'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['date'] = df['datetime'].dt.date
+    df['year'] = df['datetime'].dt.year
+    df['month_num'] = df['datetime'].dt.month
+    df['month'] = df['datetime'].dt.month_name()
+    df['day'] = df['datetime'].dt.day
+    df['day_name'] = df['datetime'].dt.day_name()
+    df['hour'] = df['datetime'].dt.hour
+    df['minute'] = df['datetime'].dt.minute
+
+    period = []
+    for hour in df[['day_name', 'hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+
+    df['period'] = period
+
+    return df
